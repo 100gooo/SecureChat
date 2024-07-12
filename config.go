f52f15b16a00e0b40e1b76a3b6ac95abe03f7784
100gooo <163338515+100gooo@users.noreply.github.com>
2024-07-12 14:10:26 +0300

    Generate file config.go

---
+++ config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+type Configuration struct {
+	ServerPort       string
+	DatabaseURL      string
+	JWTSecret        string
+}
+
+func LoadEnv() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func GetConfig() *Configuration {
+	LoadEnv()
+
+	conf := &Configuration{
+		ServerPort:       os.Getenv("SERVER_PORT"),
+		DatabaseURL:      os.Getenv("DATABASE_URL"),
+		JWTSecret:        os.Getenv("JWT_SECRET"),
+	}
+
+	return conf
+}
