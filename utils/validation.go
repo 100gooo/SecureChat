adf852f89306993a33ef69c37780f39c63c1e5dd
100gooo <163338515+100gooo@users.noreply.github.com>
2024-08-07 10:30:34 +0300

    enhance readability

--- utils/validation.go
+++ utils/validation.go
@@ -8,9 +8,23 @@ import (
 )
 
 func ValidateUsername(username string) error {
+    if err := validateUsernameLength(username); err != nil {
+        return err
+    }
+    if err := validateUsernameCharacters(username); err != nil {
+        return err
+    }
+    return nil
+}
+
+func validateUsernameLength(username string) error {
     if len(username) < 3 || len(username) > 30 {
         return errors.New("username must be between 3 and 30 characters long")
     }
+    return nil
+}
+
+func validateUsernameCharacters(username string) error {
     if match, _ := regexp.MatchString("^[a-zA-Z0-9._]+$", username); !match {
         return errors.New("username can only contain alphanumeric characters, dots, or underscores")
     }
@@ -18,13 +32,18 @@ func ValidateUsername(username string) error {
 }
 
 func ValidatePassword(password string) error {
-    minLength, _ := strconv.Atoi(os.Getenv("PASSWORD_MIN_LENGTH"))
-    maxLength, _ := strconv.Atoi(os.Getenv("PASSWORD_MAX_LENGTH"))
+    minLength, maxLength := getPasswordLengthConstraints()
     if len(password) < minLength || len(password) > maxLength {
         return errors.New("password length is out of the allowed range")
     }
     
     return nil
+}
+
+func getPasswordLengthConstraints() (minLength, maxLength int) {
+    minLength, _ = strconv.Atoi(os.Getenv("PASSWORD_MIN_LENGTH"))
+    maxLength, _ = strconv.Atoi(os.Getenv("PASSWORD_MAX_LENGTH"))
+    return minLength, maxLength
 }
 
 func ValidateEmail(email string) error {
@@ -35,9 +54,14 @@ func ValidateEmail(email string) error {
 }
 
 func ValidateMessageLength(message string) error {
-    maxLength, _ := strconv.Atoi(os.Getenv("MESSAGE_MAX_LENGTH"))
+    maxLength := getMessageMaxLength()
     if len(message) > maxLength {
         return errors.New("message exceeds the maximum allowed length")
     }
     return nil
+}
+
+func getMessageMaxLength() int {
+    maxLength, _ := strconv.Atoi(os.Getenv("MESSAGE_MAX_LENGTH"))
+    return maxLength
 }
